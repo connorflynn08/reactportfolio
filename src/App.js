c0bcf43091deeec1400a3b61ee3483f111f96d2f
Connor Flynn <connorflynn95@hotmail.com>
2021-02-15 12:11:39 +1100

    Added contact icons and corresponding links. working as intended. Added fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Body from './components/Body'
 import Splidecar from './components/Splidecar'
+import Contact from './components/Contact'
+
 
 function App() {
   return (
    <div>
      <Body />
      <Splidecar />
+     <Contact />
    </div>
      
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './contactstyles.scss'
+
+const Contact = () => {
+    return (
+        <div>
+            <div class="social-container">
+    <ul class="social-icons">
+        <li><a href="https://github.com/connorflynn08"><i class="fa fa-github"></i></a></li>
+        <li><a href="https://twitter.com/Bigconzo"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/connor-flynn-193b511b3/"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="mailto:connorflynn95@hotmail.com"><i class="fa fa-envelope"></i></a></li>
+    </ul>
+    </div>
+    <div class="footerdiv">
+        
+    </div>
+        </div>
+    )
+            
+}
+
+export default Contact
